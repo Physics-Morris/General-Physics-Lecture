1e90e5f5c711a554e115a8f692861fe6053cba48
Morris Huang <morris8934@icloud.com>
2021-10-06 18:53:51 +0800

    v1.1

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import find_packages, setup
 setup(
     name='StephCurry',
     packages=find_packages(),
-    version='1.0',
+    version='1.1',
     description='Basketball trajectory model',
     author='Morris',
     license='MIT',
